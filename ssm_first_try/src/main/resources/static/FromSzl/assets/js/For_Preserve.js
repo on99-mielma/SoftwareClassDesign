2508b3801e7e892ac15a7fcd9d9f842652548c62
on99 <akab0t@outlook.com>
2022-09-25 14:14:44 +0800

    优化预约表单

--- ssm_first_try/src/main/resources/static/FromSzl/assets/js/For_Preserve.js
+++ ssm_first_try/src/main/resources/static/FromSzl/assets/js/For_Preserve.js
@@ -34,6 +34,15 @@ const app = {
         }
     },
     methods:{
+        UTCtoGMT8(dateForm){
+            if (dateForm === "") {  //解决deteForm为空传1970-01-01 00:00:00
+                return "";
+            }else{
+                let dateee = new Date(dateForm).toJSON();
+                let date1 = new Date(+new Date(dateee)+ 8 * 3600 * 1000).toISOString().replace(/T/g,' ').replace(/\.[\d]{3}Z/,'');
+                return date1;
+            }
+        },
         getAllDpm(){
             axios({
                 url:url+"offices/2d/4",
